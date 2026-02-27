da845e9c3a94e02a1779dff8b8396d5e0945ae12
Traipop <traipopwichiansarn@gmail.com>
2026-02-27 17:17:12 +0700

    lab22 completed

--- lab22.cpp
+++ lab22.cpp
@@ -29,7 +29,59 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 }
 
 //Write your code here
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+    return ComplexNumber(real * c.real - imag * c.imag, real * c.imag + imag * c.real);
+}
 
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+    double den = pow(c.real, 2) + pow(c.imag, 2);
+    return ComplexNumber((real * c.real + imag * c.imag) / den, (imag * c.real - real * c.imag) / den);
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+    return (real == c.real && imag == c.imag);
+}
+
+double ComplexNumber::abs(){
+    return sqrt(pow(real, 2) + pow(imag, 2));
+}
+
+double ComplexNumber::angle(){
+    return atan2(imag, real) * 180 / M_PI;
+}
+
+ComplexNumber operator+(double s, const ComplexNumber &c){
+    return ComplexNumber(s + c.real, c.imag);
+}
+
+ComplexNumber operator-(double s, const ComplexNumber &c){
+    return ComplexNumber(s - c.real, -c.imag);
+}
+
+ComplexNumber operator*(double s, const ComplexNumber &c){
+    return ComplexNumber(s * c.real, s * c.imag);
+}
+
+ComplexNumber operator/(double s, const ComplexNumber &c){
+    double den = pow(c.real, 2) + pow(c.imag, 2);
+    return ComplexNumber((s * c.real) / den, (-s * c.imag) / den);
+}
+
+bool operator==(double s, const ComplexNumber &c){
+    return (s == c.real && c.imag == 0);
+}
+
+ostream & operator<<(ostream &os, const ComplexNumber &c){
+    if(c.real == 0 && c.imag == 0) return os << "0";
+    if(c.real != 0){
+        os << c.real;
+        if(c.imag > 0) os << "+" << c.imag << "i";
+        else if(c.imag < 0) os << c.imag << "i";
+    }else{
+        os << c.imag << "i";
+    }
+    return os;
+}
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
 	cout << a << "\n";
